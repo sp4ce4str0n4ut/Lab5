64fb677721d022aab58e2390c26d8338487ba52e
sp4ce4str0n4ut <56477139+sp4ce4str0n4ut@users.noreply.github.com>
2020-01-24 11:33:32 +0300

    upd

--- src/test/mergesort_test.cc
+++ src/test/mergesort_test.cc
@@ -5,7 +5,7 @@
 
 
 TEST(MERGE_SORT, REGULAR_VECTOR) {
-    std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::vector<int> v{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
     mergesort(v.begin(), v.end());
 
     for(int i = 0; i < 10; i++)
